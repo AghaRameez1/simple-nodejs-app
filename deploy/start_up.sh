c17b070546e57fdddcc7ac3b52310b7be1bc85b0
agharameez1 <agharameez1990@gmail.com>
2023-09-24 15:19:33 +0500

    from ubuntu

--- deploy/start_up.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-pm2 delete node-app
-cd ..
-pm2 start index.js --name node-app
-pm2 save
--- deploy/stop.sh
+++ deploy/stop.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-sudo echo "$pwd" > text.txt
+pm2 delete node-app
+cd ..
+pm2 start index.js --name node-app
+pm2 save
